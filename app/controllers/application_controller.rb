4e05f1f9414aa0b6d1dd77575416ee56c490694e
jmnjmnjmn <jiamengnan@gmail.com>
2015-05-22 03:46:58 -0700

    CSRF verify token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,10 @@ class ApplicationController < ActionController::Base
   before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
     # ...
-
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   protected
 
     def authorize
