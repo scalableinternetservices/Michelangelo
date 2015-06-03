c5ece3e99a7944cba408fcc7a0410498cb84c5e3
jmnjmnjmn <jiamengnan@gmail.com>
2015-06-03 13:43:06 -0700

    precompiler not fallback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,9 @@ Musicloud::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile =true
-
+  config.assets.compile =false
+  config.assets.precompile << 'sidebar.css'
+  config.assets.precompile << 'common.css'
   # Generate digests for assets URLs.
   config.assets.digest = true
 
