cffacf6eb70d82b2f002638558ad0c4042d7bed7
jmnjmnjmn <jiamengnan@gmail.com>
2015-06-02 22:36:19 -0700

    server side caching

--- app/helpers/musics_helper.rb
+++ app/helpers/musics_helper.rb
@@ -1,3 +1,10 @@
 module MusicsHelper
 	include ActsAsTaggableOn::TagsHelper
+def cache_key_for_music_table
+   "music-table-#{Music.maximum(:updated_at)}-#{Comment.maximum(:updated_at)}"
+end
+
+def cache_key_for_music_row(music)
+    "music-#{music.id}-#{music.updated_at}-#{music.comments.count}"
+end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+def cache_key_for_searchedUser_table
+    "searchedUser-table-#{User.maximum(:updated_at)}"
+end
+
+def cache_key_for_searchedUser_row(searchedUser)
+    "searchedUser-#{searchedUser.id}-#{searchedUser.updated_at}"
+end
+
+def cache_key_for_timeline_table
+    "music-table-#{Music.maximum(:updated_at)}-#{Comment.maximum(:updated_at)}"
+end
+
+def cache_key_for_timeline_row(music)
+    "music-#{music.id}-#{music.updated_at}-#{music.comments.count}"
+end
 end
